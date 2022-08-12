5b5f03193e20710a9f8d685d4dbe5f91672f0c93
safakunel <unel.safak@gmail.com>
2022-08-13 02:17:34 +0300

    Catalog.API development

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Data.Interfaces;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IMongoClient _client;
+        private readonly IMongoDatabase _database;
+        private readonly IMongoCollection<Product> _products;
+        
+        public CatalogContext(IConfiguration configuration)
+        {
+            _client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            _database = _client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            _products = _database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(_products);
+        }
+        public IMongoCollection<Product> Products 
+        { 
+            get
+            {
+                return _products;
+            }
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,33 @@
+using Catalog.API.Data;
+using Catalog.API.Data.Interfaces;
+using Catalog.API.Repositories;
+using Catalog.API.Repositories.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Context
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+// Respository
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
