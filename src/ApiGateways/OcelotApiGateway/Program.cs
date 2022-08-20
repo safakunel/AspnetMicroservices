6354fa5f80b97632473ef76ae34f640b60d625ee
safakunel <unel.safak@gmail.com>
2022-08-20 19:04:21 +0300

    Ocelot API gateway implemented.

---
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -0,0 +1,26 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
+
+builder.Host.ConfigureLogging((hostingContext, loggingbuilder) =>
+{
+    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingbuilder.AddConsole();
+    loggingbuilder.AddDebug();
+});
+
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.UseOcelot().Wait();
+
+app.Run();
